1fa357a2be99cf14a14bbcfe9636f2841971c6de
antelor <ramiro.antelo4@gmail.com>
2024-01-16 23:14:55 -0300

    added marquee

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import { Link } from "react-router-dom";
 import { Header } from "./components/Header";
+import Marquee from "react-fast-marquee";
+
 
 function App() {
   return (
-    <>
+    <div className="h-screen w-full">
       <Header />
-      <div className="flex h-screen w-full bg-red-600">
-        biem venidos
-        <Link to="cart">cart</Link>
-        <Link to="browse">browse</Link>
+      <div className="flex flex-col h-full w-full bg-rama-darker text-neutral-50">
+        adsasdasd
+        <Marquee className="bg-black max-h-fit p-6 text-8xl">Ropa fachera al mejor precio</Marquee>
       </div>
-    </>
+
+    </div>
   );
 }
 
